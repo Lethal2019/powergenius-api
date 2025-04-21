c8f01755ec237ffe0e45ec4399f26fc6e687b5ec
EmmanuelVMbewe <bed-com-20-21@unima.ac.mw>
2025-04-22 00:19:08 +0200

    front end domain update (cors)

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: 'https://powergenius.vercel.app', 
+    origin: 'https://powergeniusengineering.vercel.app', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
   });
